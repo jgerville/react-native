9fc571268bf1cf240f9e504595a6ff87e322d301
Julian Erville <jerville@gmail.com>
2022-01-07 09:20:46 -0500

    Add sample color boxes

--- nativeProject/App.js
+++ nativeProject/App.js
@@ -4,25 +4,56 @@ import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, styles.pink]}>
-        <Text>Hello, world!</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Here are some boxes of different colors
+        </Text>
+      </View>
+      <View style={[styles.colorContainer, styles.cyan]}>
+        <Text style={styles.whiteText}>Cyan #2aa198</Text>
+      </View>
+      <View style={[styles.colorContainer, styles.blue]}>
+        <Text style={styles.whiteText}>Blue #2aa198</Text>
+      </View>
+      <View style={[styles.colorContainer, styles.magenta]}>
+        <Text style={styles.whiteText}>Magenta #2aa198</Text>
+      </View>
+      <View style={[styles.colorContainer, styles.orange]}>
+        <Text style={styles.whiteText}>Orange #2aa198</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  pink: {
-    backgroundColor: 'pink',
-  },
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
   safeArea: {
     flex: 1,
   },
+  container: {
+    marginHorizontal: 30,
+    marginTop: 30,
+    marginBottom: 5,
+  },
+  colorContainer: {
+    marginHorizontal: 30,
+    marginVertical: 5,
+    paddingVertical: 10,
+    borderRadius: 3,
+    alignItems: 'center',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  whiteText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  pink: { backgroundColor: 'pink' },
+  cyan: { backgroundColor: '#2aa198' },
+  blue: { backgroundColor: '#268bd2' },
+  magenta: { backgroundColor: '#d33682' },
+  orange: { backgroundColor: '#cb4b16' },
 });
 
 export default App;
