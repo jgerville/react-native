1f6cc9d86646f46f86979de2421300f0bb53ccab
Julian Erville <jerville@gmail.com>
2022-01-08 16:18:51 -0500

    Add navigation dependencies, create two screens (without nav between them yet)

---
+++ nativeTSProject/screens/ColorPalette.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { View, Text, SafeAreaView, FlatList, StyleSheet } from 'react-native';
+import ColorBox from '../components/colorBox';
+
+const ColorPalette = () => {
+  const COLORS = [
+    { colorName: 'Base03', hexCode: '#002b36' },
+    { colorName: 'Base02', hexCode: '#073642' },
+    { colorName: 'Base01', hexCode: '#586e75' },
+    { colorName: 'Base00', hexCode: '#657b83' },
+    { colorName: 'Base0', hexCode: '#839496' },
+    { colorName: 'Base1', hexCode: '#93a1a1' },
+    { colorName: 'Base2', hexCode: '#eee8d5' },
+    { colorName: 'Base3', hexCode: '#fdf6e3' },
+    { colorName: 'Yellow', hexCode: '#b58900' },
+    { colorName: 'Orange', hexCode: '#cb4b16' },
+    { colorName: 'Red', hexCode: '#dc322f' },
+    { colorName: 'Magenta', hexCode: '#d33682' },
+    { colorName: 'Violet', hexCode: '#6c71c4' },
+    { colorName: 'Blue', hexCode: '#268bd2' },
+    { colorName: 'Cyan', hexCode: '#2aa198' },
+    { colorName: 'Green', hexCode: '#859900' },
+  ];
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Cool colors!!!</Text>
+      </View>
+      <FlatList
+        data={COLORS}
+        keyExtractor={item => item.hexCode}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} colorHex={item.hexCode} />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    marginHorizontal: 30,
+    marginTop: 30,
+    marginBottom: 5,
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  whiteText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default ColorPalette;
